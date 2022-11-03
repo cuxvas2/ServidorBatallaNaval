2df4ba13c163459871abc2aa7dc3537d4c93495c
Victor <cuevas0122@gmail.com>
2022-11-03 17:57:44 -0600

    Actualizacion de metodos

--- ServicioConWCFJuego/AdminSocial.cs
+++ ServicioConWCFJuego/AdminSocial.cs
@@ -1,26 +1,105 @@
-﻿using Entidades;
+﻿using AccesoADatos;
+using Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServicioConWCFJuego
 {
-    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant)]
-    public class AdminSocial : IAdminSocial
+    public class AdminSocial : IChat
     {
-        List<IMensajes> listaConexiones = new List<IMensajes>();
-        public void EnviarMensaje(string mensaje)
-        {
-            IMensajes contextoCliente = OperationContext.Current.GetCallbackChannel<IMensajes>();
-            foreach (IMensajes conexion in listaConexiones)
-            {
-                conexion.Respuesta(mensaje);
-            }
 
+        Dictionary<Jugador, IChatCallback> jugadores = new Dictionary<Jugador, IChatCallback>();
+
+        List<Jugador> listaJugadores = new List<Jugador>();
+
+        public IChatCallback CurrentCallback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<IChatCallback>();
+
+            }
+        }
+
+        object syncObj = new object();
+
+        public bool Conectado(Jugador jugador)
+        {
+            if (!jugadores.ContainsValue(CurrentCallback) && !buscarJugadoresPorNombre(jugador.Apodo))
+            {
+                lock (syncObj)
+                {
+                    jugadores.Add(jugador, CurrentCallback);
+                    listaJugadores.Add(jugador);
+
+                    foreach (Jugador key in jugadores.Keys)
+                    {
+                        IChatCallback callback = jugadores[key];
+                        try
+                        {
+                            callback.actualizarJugadores(listaJugadores);
+                            callback.unionDeJugador(jugador);
+                        }
+                        catch
+                        {
+                            jugadores.Remove(key);
+                            return false;
+                        }
+
+                    }
+
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public void desconectado(Jugador jugador)
+        {
+            foreach (Jugador c in jugadores.Keys)
+            {
+                if (jugador.Apodo == c.Apodo)
+                {
+                    lock (syncObj)
+                    {
+                        this.jugadores.Remove(c);
+                        this.listaJugadores.Remove(c);
+                        foreach (IChatCallback callback in jugadores.Values)
+                        {
+                            callback.actualizarJugadores(this.listaJugadores);
+                            callback.jugadorSeFue(jugador);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+
+        public void estaEscribiendo(Jugador jugador)
+        {
+            lock (syncObj)
+            {
+                foreach (IChatCallback callback in jugadores.Values)
+                {
+                    callback.escribiendoEnCallback(jugador);
+                }
+            }
+        }
+
+        private bool buscarJugadoresPorNombre(string apodo)
+        {
+            foreach (Jugador c in jugadores.Keys)
+            {
+                if (c.Apodo == apodo)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
---
+++ ServicioConWCFJuego/IAdminiSocial.cs
@@ -0,0 +1,44 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioConWCFJuego
+{
+
+        [ServiceContract(CallbackContract = typeof(IChatCallback), SessionMode = SessionMode.Required)]
+        public interface IChat
+        {
+            [OperationContract(IsInitiating = true)]
+            bool Conectado(Jugador jugador);
+
+            [OperationContract(IsOneWay = true)]
+            void estaEscribiendo(Jugador jugador);
+
+            [OperationContract(IsOneWay = true, IsTerminating = true)]
+            void desconectado(Jugador jugador);
+
+        }
+
+        [ServiceContract]
+        public interface IChatCallback
+        {
+            [OperationContract(IsOneWay = true)]
+            void actualizarJugadores(List<Jugador> jugadores);
+
+            [OperationContract(IsOneWay = true)]
+            void unionDeJugador(Jugador jugador);
+
+            [OperationContract(IsOneWay = true)]
+            void jugadorSeFue(Jugador jugador);
+            [OperationContract(IsOneWay = true)]
+            void escribiendoEnCallback(Jugador jugador);
+
+
+    }
+
+}
