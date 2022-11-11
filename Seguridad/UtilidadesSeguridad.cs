97a0ffc8f23559e796f3c256b943cda0509e59d6
omi-ttv <omarg2603@hotmail.com>
2022-11-10 18:11:33 -0600

    Creacion de Crear Sala y unirse a sala y tambien el cifrado de la contraseña

---
+++ Seguridad/UtilidadesSeguridad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seguridad
+{
+    public class Seguridad
+    {
+        public static string ComputeSHA256Hash(string contraseña)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(contraseña));
+                StringBuilder contraseñaHasheada = new StringBuilder();
+                for (int i = 0; i < (bytes.Length); i++)
+                {
+                    contraseñaHasheada.Append(bytes[i].ToString("x2"));
+                }
+                return contraseñaHasheada.ToString();
+            }
+        }
+
+        public static bool ValidarCorreoElectronico(string correoElectronico)
+        {
+            try
+            {
+                var emailValidated = new MailAddress(correoElectronico);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        public static bool ValidarContraseña(string contraseña)
+        {
+            return (contraseña.Length > 5);
+        }
+
+
+
+    }
+}
